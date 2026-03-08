ec51fdb4b7138b17f6c3d5b061861814e73396b9
zzsqjdhqgb <zzsqjdhqgb@outlook.com>
2026-03-08 21:08:07 +0800

    chore: update package name and version in package.json; set manifest name in wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
+    name: "Aria2 Browser Shim",
     permissions: [
       'downloads',
       'declarativeNetRequest',
